ddfad8ab9a04ba295f8952abc12e7a74222ebcb8
landrescreative <landres.creative@gmail.com>
2024-09-09 21:17:09 -0600

    First commit

---
+++ src/app/ui/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="w-full h-24 bg-cyan-100">
+      <div>
+        <h1 className="">800 55592283</h1>
+        <h1 className="">Soporte y Ayuda</h1>
+        <h1 className="">PREGUNTAS FRECUENTES</h1>
+        <button className="">MXN</button>
+        <button className="">ESPAÑOL</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
