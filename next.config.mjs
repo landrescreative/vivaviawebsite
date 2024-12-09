f55fbd0ed6196b7ed472b23e5adaa6ed10e800be
landrescreative <landres.creative@gmail.com>
2024-12-09 01:20:06 -0600

    testtest

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true, // Ignorar errores de TypeScript durante la compilación
+  },
+};
 
 export default withNextIntl(nextConfig);
